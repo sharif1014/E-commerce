c36d9aadcde6fc5a69b76e4642db0918d51ac6b0
sharif1014 <sharifukiuk@gmail.com>
2021-09-22 15:09:05 +0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+import Navbar from './components/Navbar';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello I am Tested for a long time</h1>
+      <Navbar />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <div>
+      <ul>
+        <li>
+          <a href="#">Home</a>
+        </li>
+        <li>
+          <a href="#">About</a>
+        </li>
+        <li>
+          <a href="#">Contact</a>
+        </li>
+        <li>
+          <a href="#">Blog</a>
+        </li>
+        <li>
+          <a href="#">Testimonials</a>
+        </li>
+        <li>
+          <a href="#">Portfolio</a>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default Navbar;
